1ffc36fd00bf8aef965f39c170c5a7aa50bd56aa
Toby Matejovsky <toby.matejovsky@gmail.com>
2010-10-20 03:38:25 +0800

    Fix implicit argument passing of super

--- lib/typed_serialize.rb
+++ lib/typed_serialize.rb
@@ -1,10 +1,10 @@
-class ActiveRecord::Base 
+class ActiveRecord::Base
   def self.typed_serialize(attr_name, class_name)
     serialize(attr_name, class_name)
 
     define_method(attr_name) do
       expected_class = self.class.serialized_attributes[attr_name.to_s]
-      if (value = super).is_a?(expected_class) 
+      if (value = super()).is_a?(expected_class)
         value
       else
         send("#{attr_name}=", expected_class.new)
